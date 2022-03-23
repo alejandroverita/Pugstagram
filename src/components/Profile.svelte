f5a606ce73ba7c8dd38f64a62cbd526b4947f6fd
Alejandro <alejandroverah97@gmail.com>
2022-03-23 10:53:12 -0500

    Header and sidebar structure

---
+++ src/components/Profile.svelte
@@ -0,0 +1,11 @@
+<div id="Profile">
+    <div class="Profile-content">
+        <div class="Profile-avatar">
+            <img src="https://arepa.s3.amazonaws.com/oscar.png" alt="">
+        </div>
+        <div class="Profile-info">
+            <h2>alejandroverita</h2>
+            <span>Alejandro Vera</span>
+        </div>
+    </div>
+</div>
---
+++ src/components/Stories.svelte
@@ -0,0 +1,19 @@
+<div id="Stories">
+    <div class="Stories-container">
+        <div class="Stories-head">
+            <h2>Stories</h2>
+            <span>Ver todas</span>
+        </div>
+        <div class="Stories-items">
+            <div class="Stories-item">
+                <div class="Stories-item-box">
+                    <img src="https://arepa.s3.amazonaws.com/oscar.png" alt="">
+                </div>
+                <h2>
+                    gndx
+                    <span>10 hours before</span>
+                </h2>
+            </div>
+        </div>
+    </div>
+</div>
