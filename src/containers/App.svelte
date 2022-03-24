bcf14ef131c4b56c0d8de5dc5eccb580e9d8f30c
Alejandro <alejandroverah97@gmail.com>
2022-03-24 11:00:44 -0500

    Styles

--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -5,9 +5,12 @@
     import Sidebar from '../components/Sidebar.svelte';
 </script>
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
     :global(body){
-        background-color: #676778;
-        color: #FF8987
+        background-color: #fafafa;
+        color: rgba(38,38,38,0.7);
+        font-family: "Lato", sans-serif;
     }
 </style>
 
