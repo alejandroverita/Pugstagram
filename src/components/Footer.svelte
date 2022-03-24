ce189996513d08f2554b14494ff44a614991563a
Alejandro <alejandroverah97@gmail.com>
2022-03-24 10:26:13 -0500

    Footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,10 @@
+<script>
+    let actualYear = new Date().getFullYear();
+</script>
+
+<div id="Footer">
+    <div class="Footer-container">
+        <p>© {actualYear} Todos los derechos reservados</p>
+        <p>Pugstagram from <strong>Platzi</strong></p>
+    </div>
+</div>
