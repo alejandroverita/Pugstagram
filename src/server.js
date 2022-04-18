53fcbe1f1e0366c2526fb70aaf2157a1032a37bf
Alejandro <alejandroverah97@gmail.com>
2022-04-18 11:01:47 -0500

    Install and config sapper

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import sirv from 'sirv';
+import compression from 'compression';
+import polka from 'polka';
+import * as sapper from '@sapper/server';
+
+//call node_env
+const { NODE_ENV } = process.env;
+const dev = NODE_ENV === 'development';
+
+//start server
+polka()
+    .use(
+        compression({threshold: 0}),
+        sirv('static', {dev} ), //handle static
+        sapper.middleware()
+    )
+    .listen(PORT, err => {
+        if(err) console.log('Ha habido un error', err);
+    });
