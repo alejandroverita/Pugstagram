c815da2c55547d98179c193b6936d61b7d2abfd3
Alejandro <alejandroverah97@gmail.com>
2022-04-19 11:12:46 -0500

    config layout

---
+++ src/routes/index.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Header from '../components/Header.svelte';
+    import Main from '../components/Main.svelte';
+    import TimeLine from '../components/TimeLine.svelte';
+    import Sidebar from '../components/Sidebar.svelte';
+    
+    import { onMount } from "svelte";
+    import { isDarkMode } from "../store/store";
+
+    let data = {};
+    const API = "https://api-pugstagram.herokuapp.com/?nickname=alejandroverita&name=Alejandro%20Vera"
+
+    onMount(async () => {
+        const response = await fetch(API)
+        data = await response.json();
+    });
+
+    isDarkMode.subscribe(value => {
+        value 
+            ? document.body.classList.add("dark")
+            : document.body.classList.remove("dark");
+    })
+
+</script>
+
+<Header />
+<Main>
+    <TimeLine posts = { data.posts} />
+    <Sidebar {...data.user}/>
+</Main>
